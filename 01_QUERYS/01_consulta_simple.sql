7c6485b391df8c72d24691be3444a69f18fd0694
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-05-02 20:34:52 -0500

    Query inicial

---
+++ 01_QUERYS/01_consulta_simple.sql
@@ -0,0 +1,3 @@
+SELECT PERSONA, TELEFONO, CELULAR FROM CLIENTES;
+
+-- Selecciona las columnas persona, telefono, celular de la tabla clientes
---
+++ Teoria/02_instrucciones.ts
@@ -0,0 +1,11 @@
+/**
+ * 
+ * INSTRUCCIONES SQL
+ * 
+ * - Las instrucciones SQL suelen tener Comando + clausulas + operadores + funciones
+ *  ( No siempre suelen tener este tipo complegidad )
+ * 
+ * 
+ * 
+ * 
+*/
