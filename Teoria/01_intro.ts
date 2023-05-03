8cc91e014657b817175c85a3ffeb76dcfc3828c8
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-05-02 20:23:09 -0500

    Intro | Comandos SQL

---
+++ Teoria/01_intro.ts
@@ -0,0 +1,44 @@
+/**
+ * 
+ * ESTANDAR ASI - ISO 
+ * 
+ * 
+ * - COMANDOS SQL
+ * 
+ * -- Data Definition Lenguage DDL
+ * --- CREATE
+ * --- ALTER
+ * --- DROP 
+ * --- TRUNCATE
+ * 
+ * 
+ * -- DML Consultas
+ * --- SELECT
+ * --- UPDATE
+ * --- INSERT 
+ * --- DELETE 
+ * 
+ * 
+ * -- DCL Proporcionar seguridad
+ * --- GRANT 
+ * --- REVOKE
+ * 
+ * -- TCL Gestion de los cambios en los datos
+ * --- COMMIT 
+ * --- ROLLBACK 
+ * --- SAVEPOINT
+ * 
+ * 
+ * 
+ * CLAUSULAS
+ * 
+ * - FROM
+ * - WHERE 
+ * - GROUP BY
+ * - HAVING 
+ * - ORDER BY
+ * 
+ * 
+
+ * 
+ * */ 
