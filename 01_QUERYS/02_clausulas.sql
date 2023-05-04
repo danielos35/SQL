f91562aba2203c725d3ab83e59db45b1d3a06c78
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-05-03 20:37:18 -0500

    Clausulas

---
+++ 01_QUERYS/02_clausulas.sql
@@ -0,0 +1,11 @@
+-- WHERE
+-- - Es utilizada para especificar condiciones y criterios
+
+-- GOUP BY
+--- Agrupar registros seleccionados en función del campo
+
+-- HAVING
+--- Especifica las condiciones o criterios que deben de cumplir los grupos
+
+-- ORDER BY
+--- Ordena los registros seleccionados en función de un campo
