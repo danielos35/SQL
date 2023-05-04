4702d5d3fa702d17ae41e89cd1a37e7348ff0979
Daniel Márquez <85912134+danielos35@users.noreply.github.com>
2023-05-03 21:02:05 -0500

    Query datos

--- 01_QUERYS/01_consulta_simple.sql
+++ 01_QUERYS/01_consulta_simple.sql
@@ -1,3 +1,11 @@
 SELECT PERSONA, TELEFONO, CELULAR FROM CLIENTES;
 
 -- Selecciona las columnas persona, telefono, celular de la tabla clientes
+
+-- Consulta de deportes Y deportes, consulta solo los que cumplan con ambos criterios
+SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO FROM PRODUCTOS WHERE  SECCIÓN ="CERÁMICA" AND SECCIÓN = "DEPORTES"
+
+-- Consulta todos los que cumplan con el criterio
+SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO FROM PRODUCTOS WHERE  SECCIÓN ="CERÁMICA" OR SECCIÓN = "DEPORTES"
+
+
